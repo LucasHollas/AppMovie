98bf0764d88ec13f5c30a81332ced1cdb12dd301
lucashollas <devlucashollas@gmail.com>
2022-12-17 19:58:40 -0300

    Initial Commit

---
+++ AppMovie/AppMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppMovieApp.swift
+//  AppMovie
+//
+//  Created by Lucas Hollas on 17/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AppMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
