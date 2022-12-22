1223a8f2b3053992d55b1cdce402e597abfdc119
lucashollas <devlucashollas@gmail.com>
2022-12-21 22:04:59 -0300

    Implementação de classe Utils

--- AppMovie/MovieStore.swift
+++ AppMovie/MovieStore.swift
@@ -8,11 +8,58 @@
 import Foundation
 
 class MovieStore: MovieService {
-    
+
     static let shared = MovieStore()
     private init() {}
     
     private let apiKey = "APIKEY"
     private let baseAPIURL = "https://api.themoviedb.org/3"
-    private let urlSessioni = URLSession.shared
+    private let urlSession = URLSession.shared
+    private let jsonDecoder = Utils.jsonDecoder
+    
+    func fetchMovies(from endpoint: MovieListEndpoint, completion: @escaping (Result<MovieResponse, MovieError>) -> ()) {
+        
+    }
+    
+    func fetchMovie(id: Int, completion: @escaping (Result<Movie, MovieError>) -> ()) {
+        
+    }
+    
+    func searchMovie(query: String, completion: @escaping (Result<MovieResponse, MovieError>) -> ()) {
+        
+    }
+    
+    private func loadURLAndDecode<D: Decodable>(url: URL, params: [String: String]? = nil, completion: @escaping (Result<D, MovieError>) -> ()) {
+        guard var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false) else {
+            completion(.failure(.invalidEndpoint))
+            return
+        }
+        
+        var queryItems = [URLQueryItem(name: "api-key", value: apiKey)]
+        if let params = params {
+            queryItems.append(contentsOf: params.map { URLQueryItem(name: $0.key, value: $0.value) })
+        }
+        
+        urlComponents.queryItems = queryItems
+        
+        guard let finalURL = urlComponents.url else {
+            completion(.failure(.invalidEndpoint))
+            return
+        }
+        
+        urlSession.dataTask(with: finalURL) { (data, response, error) in
+            if error != nil {
+                completion(.failure(.apiError))
+            }
+            
+        }
+    
+    }
+    
+    private func executeCompletionHandlerInMainThread<D: Decodable>(with result: Result<D, MovieError>, completion: @escaping (Result<D, MovieError>) -> ()) {
+        DispatchQueue.main.async {
+            completion(result)
+        }
+    }
+    
 }
---
+++ AppMovie/Utils.swift
@@ -0,0 +1,24 @@
+//
+//  Utils.swift
+//  AppMovie
+//
+//  Created by Lucas Hollas on 21/12/22.
+//
+
+import Foundation
+
+class Utils {
+    
+    static let jsonDecoder: JSONDecoder = {
+        let jsonDecoder = JSONDecoder()
+        jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        jsonDecoder.dateDecodingStrategy = .formatted(dateFormatter)
+        return jsonDecoder
+    }()
+    
+    static let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-mm-dd"
+        return dateFormatter
+    }()
+}
