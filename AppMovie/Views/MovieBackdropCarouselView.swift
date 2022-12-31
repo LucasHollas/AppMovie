ce0f037c500fcd6a37144887d89ce20c1f73ef33
lucashollas <devlucashollas@gmail.com>
2022-12-31 17:57:11 -0300

    Apenas a criação do arquivo de carousel

---
+++ AppMovie/Views/MovieBackdropCarouselView.swift
@@ -0,0 +1,24 @@
+//
+//  MovieBackdropCarouselView.swift
+//  AppMovie
+//
+//  Created by Lucas Hollas on 31/12/22.
+//
+
+import SwiftUI
+
+struct MovieBackdropCarouselView: View {
+    
+    let title: String
+    let movies:[Movie]
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieBackdropCarouselView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieBackdropCarouselView()
+    }
+}
