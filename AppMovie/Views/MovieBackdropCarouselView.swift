6209829eb34053b4ade16f4de3afa41a9cf85d25
lucashollas <devlucashollas@gmail.com>
2023-01-02 21:38:49 -0300

    Método capa carousel

--- AppMovie/Views/MovieBackdropCarouselView.swift
+++ AppMovie/Views/MovieBackdropCarouselView.swift
@@ -10,15 +10,26 @@ import SwiftUI
 struct MovieBackdropCarouselView: View {
     
     let title: String
-    let movies:[Movie]
+    let movies: [Movie]
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 0) {
+            Text(title)
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.horizontal)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 16) {
+                    ForEach(self.movies)
+                }
+            }
+        }
     }
 }
 
 struct MovieBackdropCarouselView_Previews: PreviewProvider {
     static var previews: some View {
-        MovieBackdropCarouselView()
+        MovieBackdropCarouselView(title: "Latest", movies: Movie .stubbedMovies)
     }
 }
