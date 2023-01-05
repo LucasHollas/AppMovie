cc7868af8d87d2ad070d44bc7b81cbab825020f9
lucashollas <devlucashollas@gmail.com>
2023-01-04 21:39:22 -0300

    Término do método porém o retorno é de inválido response

--- AppMovie/Views/MovieListView.swift
+++ AppMovie/Views/MovieListView.swift
@@ -17,23 +17,69 @@ struct MovieListView: View {
     var body: some View {
         NavigationView {
             List {
-                if nowPlayingState.movies != nil {
-                    MoviePosterCarouselView(title: "Now Playing", movies: nowPlayingState.movies!)
-                } else {
-                    LoadingView(isLoading: nowPlayingState.isLoading, error: nowPlayingState.error) {
-                        self.nowPlayingState.loadMovies(with: .nowPlaying)
+                Group {
+                    if nowPlayingState.movies != nil {
+                        MoviePosterCarouselView(title: "Now Playing", movies: nowPlayingState.movies!)
+                        
+                    } else {
+                        LoadingView(isLoading: self.nowPlayingState.isLoading, error: self.nowPlayingState.error) {
+                            self.nowPlayingState.loadMovies(with: .nowPlaying)
+                        }
                     }
+                    
                 }
+                .listRowInsets(EdgeInsets(top: 16, leading: 0, bottom: 16, trailing: 0))
                 
-                if upcomingState.movies != nil {
-                    MovieBackdropCarouselView(title: "Upcoming", movies: upcomingState.movies!)
-                } else {
-                    LoadingView(isLoading: upcomingState.isLoading, error: upcomingState.error) {
-                        self.upcomingState.loadMovies(with: .nowPlaying)
+                Group {
+                    if upcomingState.movies != nil {
+                        MovieBackdropCarouselView(title: "Upcoming", movies: upcomingState.movies!)
+                    } else {
+                        LoadingView(isLoading: self.upcomingState.isLoading, error: self.upcomingState.error) {
+                            self.upcomingState.loadMovies(with: .upcoming)
+                        }
                     }
                 }
+                .listRowInsets(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
+                
+                
+                Group {
+                    if topRatedState.movies != nil {
+                        MovieBackdropCarouselView(title: "Top Rated", movies: topRatedState.movies!)
+                        
+                    } else {
+                        LoadingView(isLoading: self.topRatedState.isLoading, error: self.topRatedState.error) {
+                            self.topRatedState.loadMovies(with: .topRated)
+                        }
+                    }
+                    
+                    
+                }
+                .listRowInsets(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
+                
+                Group {
+                    if popularState.movies != nil {
+                        MovieBackdropCarouselView(title: "Popular", movies: popularState.movies!)
+                        
+                    } else {
+                        LoadingView(isLoading: self.popularState.isLoading, error: self.popularState.error) {
+                            self.popularState.loadMovies(with: .popular)
+                        }
+                    }
+                }
+                .listRowInsets(EdgeInsets(top: 8, leading: 0, bottom: 16, trailing: 0))
+                
+                
+                
             }
+            .navigationBarTitle("The MovieDb")
         }
+        .onAppear {
+            self.nowPlayingState.loadMovies(with: .nowPlaying)
+            self.upcomingState.loadMovies(with: .upcoming)
+            self.topRatedState.loadMovies(with: .topRated)
+            self.popularState.loadMovies(with: .popular)
+        }
+        
     }
 }
 
