51e6853d5e0bcb813750263d873b6c572c2ebacb
lucashollas <devlucashollas@gmail.com>
2023-01-06 00:36:53 -0300

    Feat(MovieDetailView): Criação inicial do arquivo

---
+++ AppMovie/Views/MovieDetailView.swift
@@ -0,0 +1,23 @@
+//
+//  MovieDetailView.swift
+//  AppMovie
+//
+//  Created by Lucas Hollas on 06/01/23.
+//
+
+import SwiftUI
+
+struct MovieDetailView: View {
+    
+    let movieId: Int
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieDetailView()
+    }
+}
